df569da2b1b4ef6c289bdee37170e4d60f2a06ad
kiril <xalkovich@gmail.com>
2026-04-18 21:18:20 +0300

    update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,6 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
+set -g fish_key_bindings fish_vi_key_bindings
+set -g fish_sequence_key_delay_ms 500
 
 if status is-interactive
     source ~/.config/fish/my/keybinds.fish
--- fish/.config/fish/my/keybinds.fish
+++ fish/.config/fish/my/keybinds.fish
@@ -1,11 +1,19 @@
 function fish_user_key_bindings
+    # Бинды для режима вставки (insert mode)
+    bind -M insert \cl accept-autosuggestion
+    bind -M insert \el forward-word
+
     # Принятие автодополнения (autosuggestion) через Ctrl+L
-    bind \cl accept-autosuggestion
+    bind -M default \cl accept-autosuggestion
+
+    # Переход в нормальный режим по 'jk '
+    bind -M insert -m default jk backward-char
 
     # Принять только одно слово (Alt + L)
     bind \el forward-word
 
     # Fzf
+    bind -M insert \cf "_fzf_search_directory; kitty +kitten icat --clear"
     bind \cf "_fzf_search_directory; kitty +kitten icat --clear"
 end
 
