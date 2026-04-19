5bcd2f6a139a15e784b2887875732cd9cb9b8231
kiril <xalkovich@gmail.com>
2026-04-19 20:48:28 +0300

    update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,4 +7,16 @@ if status is-interactive
     source ~/.config/fish/my/fzf.fish
 end
 
+if status is-interactive
+    zoxide init fish | source
+end
+
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
 
